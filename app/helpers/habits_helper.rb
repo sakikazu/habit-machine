9c704a3ca05d72999dc6a4a0dfd946da7f4e6b35
Kazutaka Sakimura <sakikazu15@gmail.com>
2012-11-23 02:13:26 +0900

    best_in_placeで登録した値が表示されなかったバグ修正

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -7,6 +7,8 @@ module HabitsHelper
   def input_value_for_inline_edit(record)
     if record.habit.value_type == 1
       best_in_place record, :value, nil: "未入力", display_as: :formatted_value, type: :select, collection: Record::SYMBOLIC_VALUE
+      # todo ちゃんとヘルパーはimage_tagを出力しているのに画像が表示されない
+      # best_in_place record, :value, nil: "未入力", display_with: lambda{|v| result_icon2(v).html_safe}, type: :select, collection: Record::SYMBOLIC_VALUE
     else
       best_in_place record, :value, nil: "未入力", display_as: :formatted_value
     end
@@ -23,4 +25,10 @@ module HabitsHelper
       record.formatted_value
     end
   end
+
+  # todo display_withのお試し用
+  # def result_icon2(value)
+      # icon_set = ["Sun.png", "db1s.png", "db3s.png", "db5s.png", "db7s.png"]
+      # image_tag("result_table/#{icon_set[value.to_i - 1]}")
+  # end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -7,7 +7,7 @@ class Record < ActiveRecord::Base
   belongs_to :habit
 
   # best_in_placeで扱うcollectionの形が、Railsの普通のselectとは表示値と値が逆だった
-  SYMBOLIC_VALUE = [[nil, ""], [1, "1"], [2, "2"], [3, "3"], [4, "4"], [5, "5"]]
+  SYMBOLIC_VALUE = [[nil, ""], [1.0, "1"], [2.0, "2"], [3.0, "3"], [4.0, "4"], [5.0, "5"]]
 
 
   def self.find_or_create(habit_id, record_at)
