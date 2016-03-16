9d9fe0a7a81fffd1f4b71c20cf44c9ef31c76606
sakikazu <sakikazu15@gmail.com>
2016-03-16 22:03:02 +0900

    一覧画面でも日記のハイライト・シークレットがわかるようにした

--- app/helpers/diaries_helper.rb
+++ app/helpers/diaries_helper.rb
@@ -13,6 +13,9 @@ module DiariesHelper
     output = ""
     tags = tags_link(diary)
 
+    output += "<span class='label label-danger'>ｼｰｸﾚｯﾄ</span>".html_safe if diary.is_secret
+    output += "<span class='label label-warning'>ﾊｲﾗｲﾄ</span>".html_safe if diary.is_hilight
+    output += "<br>".html_safe if diary.is_hilight or diary.is_secret
     output += tags + "<br>".html_safe if tags.present?
     output += "<strong>" + (link_to diary.title_mod, diary_path(diary)) + "</strong>"
     output += " " + (link_to "[edit]", edit_diary_path(diary)) + "<br>"
