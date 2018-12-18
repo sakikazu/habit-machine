225f06586a6020d939e50b0be2c50f0685b101c5
k.sakimura <sakikazu15@gmail.com>
2018-12-18 11:17:23 +0900

    bootstrapでのデザインをちゃんとした

--- app/helpers/diaries_helper.rb
+++ app/helpers/diaries_helper.rb
@@ -13,8 +13,8 @@ module DiariesHelper
     output = ""
     tags = tags_link(diary)
 
-    output += "<span class='label label-danger'>ｼｰｸﾚｯﾄ</span>".html_safe if diary.is_secret
-    output += "<span class='label label-warning'>ﾊｲﾗｲﾄ</span>".html_safe if diary.is_hilight
+    output += "<span class='badge badge-danger'>ｼｰｸﾚｯﾄ</span>".html_safe if diary.is_secret
+    output += "<span class='badge badge-warning'>ﾊｲﾗｲﾄ</span>".html_safe if diary.is_hilight
     output += "<br>".html_safe if diary.is_hilight or diary.is_secret
     output += tags + "<br>".html_safe if tags.present?
     output += "<strong>" + (link_to diary.title_mod, diary_path(diary)) + "</strong>"
