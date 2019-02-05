4cb04adc937490487a6d453645391e0d06c3d3e0
k.sakimura <sakikazu15@gmail.com>
2019-02-05 17:54:53 +0900

    エラー画面を設けた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  class Forbidden < ActionController::ActionControllerError; end
+  class NotFound < ActionController::ActionControllerError; end
+  include ErrorHandlers if Rails.env.production?
+
   layout :select_layout
   before_action :configure_permitted_parameters, if: :devise_controller?
 
