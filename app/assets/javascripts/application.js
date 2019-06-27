21a5ab89c8ff7778e7ff1c76bb71f4e8b767e9bd
k.sakimura <sakikazu15@gmail.com>
2019-06-27 18:01:27 +0900

    best_in_placeのtextarea値が、turbolinksの遷移で戻った場合に改行が<br>に変換されたものとなってしまうため修正 そのため、Recordのメモの表示をセルクリックでPopover表示するなど色々変更した

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,33 +32,57 @@
 //= require highcharts/highstock
 
 $(document).on('turbolinks:load', function() {
-	// best_in_place内のpopoverを表示させてそのtextareaにフォーカスすると、popoverが表示されたままになってしまう問題の対処
-	$('table.hm_top td.record').on('click', function() {
-		$('.popover').hide();
-	});
-
   $('[data-toggle="popover"]').popover({
     html: true,
   });
-
   $("a[rel=tooltip]").tooltip();
+  $('.best_in_place').best_in_place()
 
-	$('.best_in_place').best_in_place()
-	// textareaタイプの場合、turbolinksの遷移から戻った後に変更内容が失われる問題の対処
-	$('.best_in_place').bind("ajax:success", function() {
-		if ($(this).data('bip-type') != 'textarea') {
-			return
-		}
-		// 正しい値はspan.best_in_place > aが持っている
-		var updatedValue = $(this).children('a').first().data('content');
-		// data-bip-original-contentを書き換えると、遷移後も値が正しいままとなる
-		// data()メソッドでは書き換えができなかった
-		$(this).attr('data-bip-original-content', updatedValue);
-		// popoverが古い値もまま表示されたままになる問題があり、消すこともできないので、更新された値で再度表示できるようにする
-		$(this).children('a').popover('update');
-	});
+  //
+  // for best_in_place
+  //
+  var shownPopoverElements = [];
+  // セルをクリック時、recordのメモをポップアップ表示する
+  // bipの入力フォームの場合はポップアップはせず、現在表示されているポップアップを非表示にする
+  $('table.hm_top td.record').on('click', function(e) {
+    if($(e.target).hasClass('record')) {
+      var $ele = $(this).find('.habit-data');
+      hidePopover(shownPopoverElements, $ele);
+      $ele.popover('toggle');
+      shownPopoverElements.push($ele);
+    } else {
+      hidePopover(shownPopoverElements);
+    }
+  });
+
+  // bipがtextareaタイプの場合、turbolinksの遷移から戻った後に変更内容が失われる問題の対処
+  // NOTE: 理由：bipは変更確定時、データをポストし、表示用であるdisplay_withの関数にのみ、変更後の値を保持するようだ
+  //             でも、turbolinksで戻った時、textareaの値は、data-bip-original-content属性からセットされるので、
+  //             diaplay_withの中の値をdata-bip-original-contentにセットすることで、正しくtextarea値をセットすることができる
+  // NOTE: この中ではdata-*属性はdata()メソッドで書き換えできなかった。毎度datasetを使った方が良さそう
+  $('.best_in_place').bind("ajax:success", function() {
+    if ($(this).data('bip-type') != 'textarea') { return }
+
+    // best_in_placeのtextarea編集を完了するには他の要素をクリックする必要があるが、
+    // その際にPopoverが表示されてしまうので、それをこのタイミングで非表示にする
+    hidePopover(shownPopoverElements);
+
+    // 正しい値はspan.best_in_place > spanが持っている(display_withの中)
+    var updatedValue = $(this).children('span').first().data('content');
+    // 表示用の値を変更後のものにする
+    $(this).parents('.record').children('.habit-data')[0].dataset.content = nl2br(updatedValue);
+    // data-bip-original-contentを書き換えると、遷移後も値が正しいままとなる
+    this.dataset.bipOriginalContent = updatedValue;
+  });
 })
 
+hidePopover = function(elements, $withoutEle = null) {
+  elements.forEach(function($popEle) {
+    if ($withoutEle == null || !$popEle.is($withoutEle)) {
+      $popEle.popover('hide');
+    }
+  });
+}
 
 // Diaryのフォームのタグフィールドに選択したタグ名をトグルで追加/削除する
 toggleTag = function(tagname) {
@@ -88,4 +112,10 @@ showCurrentSenses = function() {
       $('#current-senses.modal').modal('show');
     }
   });
+}
+
+nl2br = function(str) {
+  str = str.replace(/\r\n/g, "<br />");
+  str = str.replace(/(\n|\r)/g, "<br />");
+  return str;
 }
--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -3,33 +3,30 @@ module HabitsHelper
   #
   # best_in_placeでインライン編集
   #
-  def input_value_for_inline_edit(record, habit_value_type)
+  def best_in_place_wrapper(record, habit_value_type)
     action_url = url_for(controller: :records, action: :update_or_create, habit_id: record.habit_id, record_at: record.record_at)
+    options = { place_holder: "未入力", url: action_url, classes: 'text-danger' }
 
     # セレクトボックス形式
     if habit_value_type == Habit::VALUE_TYPE_COLLECTION
-      best_in_place record, :value, place_holder: "未入力", as: :select, collection: Record::SYMBOLIC_VALUE, url: action_url
+      options.merge!(as: :select, collection: Record::SYMBOLIC_VALUE)
       # todo ちゃんとヘルパーはimage_tagを出力しているのに画像が表示されない
-      # best_in_place record, :value, place_holder: "未入力", display_with: lambda{|v| result_icon2(v).html_safe}, as: :select, collection: Record::SYMBOLIC_VALUE
+      # display_with: lambda{|v| result_icon2(v).html_safe}, as: :select, collection: Record::SYMBOLIC_VALUE
 
     # 自由入力形式
     else
-      best_in_place record, :value, place_holder: "未入力", display_with: lambda {|v| habit_value_type == Habit::VALUE_TYPE_INT ? v.to_i : v}, url: action_url
+      options.merge!(display_with: lambda {|v| habit_value_type == Habit::VALUE_TYPE_INT ? v.to_i : v})
     end
+
+    best_in_place record, :value, options
   end
 
-  #
-  # 記録データのメモ表示(best_in_place用)
-  #
-  def link_including_icon_for_bip(memo)
-    return "" if memo.blank?
-    icon = fa_icon 'comment'
-    link_to icon.html_safe, "#", data: {
-      toggle: 'popover',
-      trigger: 'hover',
-      html: true,
-      content: nl2br(memo).html_safe
-    }
+  def best_in_place_textarea_content(val)
+    if val.present?
+      content_tag('span', fa_icon('comment'), data: { content: val })
+    else
+      fa_icon('comment-o')
+    end
   end
 
   #
