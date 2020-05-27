4192118fd6d49f498f71a71563a0dda7adfc344b
k.sakimura <sakikazu15@gmail.com>
2020-05-27 18:46:37 +0900

    WebpackでVue.js - habitodos.jsをjavascript_pack_tagを使って読み込み - const, letが必須になったので追加 - メリットは、今後Vue.jsのSFCが使えるようになったこと、その他はまだわからん - デメリットは、js編集したらリビルディングで時間がかかるようになったこと

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,7 +8,8 @@
 import Vue from 'vue'
 import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
+//document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
     render: h => h(App)
   }).$mount()
