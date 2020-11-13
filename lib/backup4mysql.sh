112bd73fc06f1b4a67f9900a57f37faaab9750f2
k.sakimura <sakikazu15@gmail.com>
2020-11-13 18:50:05 +0900

    バックアップシェルを修正

--- lib/backup4mysql.sh
+++ lib/backup4mysql.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 db='habitm'
 user='root'
 password_file='/usr/local/site/habit-machine/current/config/mysqldump.ini'
@@ -23,3 +22,6 @@ mysqldump --defaults-extra-file=$password_file -u $user $db | gzip -c > $dirpath
 # 古いバックアップファイルを削除
 oldfile="$db"_`date --date "$period days ago" +%y%m%d`
 rm -f $dirpath/$oldfile.sql.gz
+
+# このシェルを実行したプログラムでバックアップファイル名を取得したい時のために、標準出力する
+echo $dirpath/$filename.sql.gz
