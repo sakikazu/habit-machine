65acfd21dec52b8c44c61210299a63a4909b1893
k.sakimura <sakikazu15@gmail.com>
2021-09-24 23:41:58 +0900

    tiny fix

--- app/models/custom_redcarpet_render.rb
+++ app/models/custom_redcarpet_render.rb
@@ -1,8 +1,7 @@
 class CustomRedcarpetRender < Redcarpet::Render::HTML
   def header(text, header_level)
     if text == Diary::ACTION_MEMO_LINE.gsub(/^#+\s+/, '')
-      # NOTE: パーセントで囲むとhtmlがそのまま書けるのか。使える
-      %(<h#{header_level} class="action_memo_in_md">追加されたメモ</h#{header_level}>)
+      %(<h#{header_level} class="action_memo_in_md">メモ追加欄</h#{header_level}>)
     else
       %(<h#{header_level}>#{text}</h#{header_level}>)
     end
