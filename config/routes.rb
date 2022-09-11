3e18e1723b309556ec43269fc4e249f93a837e3c
k.sakimura <sakikazu15@gmail.com>
2022-09-11 18:47:32 +0900

    deviseをupdateして関連ファイルも最新に。パスワード再設定ができないバグはviewの情報が古いのが原因だった

--- config/routes.rb
+++ config/routes.rb
@@ -122,11 +122,11 @@ Rails.application.routes.draw do
 
 	# devise_for :admins
 
-	devise_for :users, skip: [:registrations]
-	as :user do
-		get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-		put 'users' => 'devise/registrations#update', :as => 'user_registration'
-	end
+  devise_for :users, skip: [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   resources :diaries do
     member do
