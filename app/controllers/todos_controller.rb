16df835f179c528bc973bfc74f958c8069c2587a
k.sakimura <sakikazu15@gmail.com>
2022-10-17 08:05:49 +0900

    Todo.upsert_all を使って書き直し！

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -47,13 +47,16 @@ class TodosController < ApplicationController
     end
   end
 
-  # TODO: これだとupdateが多すぎる。rails6にしてupsert_allを使いたい
+  # ブロックを移動したら、移動先と移動元の2回分呼ばれる
   def bulk_update
-    items = params[:items]
-    items.each do |item|
-      todo = current_user.todos.find(item[:id])
-      todo.update(priority: item[:priority], sort_order: item[:sort_order])
+    items = todos_moving_params
+    ids = items.map { |item| item[:id] }
+    todos = current_user.todos.find(ids)
+    todos_attributes = todos.map do |todo|
+      updating_attrs = items.find { |item| item[:id] == todo.id }
+      todo.attributes.merge(updating_attrs).merge(updated_at: Time.zone.now)
     end
+    Todo.upsert_all(todos_attributes)
     # TODO: 成功かどうかを返せばいいだけ
     @todos = current_user.todos.order(sort_order: :asc)
     render :index
@@ -90,5 +93,9 @@ class TodosController < ApplicationController
     # Only allow a list of trusted parameters through.
     def todo_params
       params.require(:todo).permit(:title, :content, :priority, :project_id)
+    end
+
+    def todos_moving_params
+      params.fetch(:items, []).map { |item| item.permit(:id, :priority, :sort_order) }
     end
 end
