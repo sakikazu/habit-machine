883ae09efd4f0e5e3fa18be5435c45c8d8543979
k.sakimura <sakikazu15@gmail.com>
2025-01-13 15:24:00 +0900

    ちょっとしたデザイン変更

--- app/javascript/hm_axios.js
+++ app/javascript/hm_axios.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 export default axios.create({
   headers: {
+    // TODO: 一般的なAPIのセキュリティと思われるAPIキーやJWTへの代替を検討したい。rails依存をなくすくらいのメリット？
+    // csrf_meta_tagsヘルパーが出力するcsrf-tokenを読み取る
     'X-CSRF-TOKEN': document.getElementsByName('csrf-token')[0].content,
     // TODO: ここでjsonにしていても、URLパスに.jsonをつけないとjson形式にならない。RailsのRoutesが優先されるってことかな？
     'Content-Type': 'application/json'
