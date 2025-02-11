2be783ea4e0fa3adb192a503358b7bbc1e45caaa
k.sakimura <sakikazu15@gmail.com>
2025-02-11 09:51:51 +0900

    カテゴリ表示時に共有アイコンをつけた

--- app/javascript/components/categories/CategoryLinks.vue
+++ app/javascript/components/categories/CategoryLinks.vue
@@ -2,8 +2,9 @@
   <div class="diary-categories">
     <span>カテゴリ:</span>
     <div class="category-list">
-      <a v-for="(category, idx) in savedCategories" :key="idx" href="/categories" class="category-link">
-        {{ category }}
+      <a v-for="category in savedCategories" :key="category.id" href="/categories" class="category-link">
+        <i v-if="category.shared" class="fa fa-users mr-2" style="color:orange"></i>
+        {{ category.path.join(' > ') }}
       </a>
     </div>
     <button class="btn btn-light" style="min-width: 70px" @click="$emit('edit-category')">編集</button>
@@ -21,7 +22,9 @@ export default {
       const findCategoryPath = (categories, targetId, path = []) => {
         for (const category of categories) {
           const newPath = [...path, category.name];
-          if (category.id === targetId) return newPath;
+          if (category.id === targetId) {
+            return { path: newPath, id: category.id, shared: category.shared };
+          }
           if (category.children) {
             const found = findCategoryPath(category.children, targetId, newPath);
             if (found) return found;
@@ -31,7 +34,7 @@ export default {
       };
 
       return this.savedCategoryIds
-        .map(id => findCategoryPath(this.categories, id)?.join(' > '))
+        .map(id => findCategoryPath(this.categories, id))
         .filter(Boolean);
     }
   }
